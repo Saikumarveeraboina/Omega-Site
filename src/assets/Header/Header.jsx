3543e415e8265447012edd164a5dec493fe5e57d
Saikumarveeraboina <saikumar96030433@gmail.com>
2025-12-08 15:30:13 +0530

    added files

--- src/assets/Header/Header.jsx
+++ src/assets/Header/Header.jsx
@@ -1,38 +1,50 @@
-import React , {useState} from 'react'
-import {Link, NavLink} from 'react-router-dom'
-import logo from '../../Images/logo-vsk-incl-e.png'
-import './Header.css'
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../Images/logo-vsk-incl-e.png";
+import "./Header.css";
 
 const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <nav>
-      <Link to="/"> <img src={logo} alt="logo" className='website-logo'/> </Link>
-      <div className='menu' onClick={() => setIsMenuOpen(!isMenuOpen)}>
+    <nav className="navbar">
+      {/* Logo */}
+      <Link to="/">
+        <img src={logo} alt="logo" className="website-logo" />
+      </Link>
+
+      {/* Hamburger */}
+      <div className="menu" onClick={() => setIsMenuOpen(!isMenuOpen)}>
         <span></span>
         <span></span>
         <span></span>
       </div>
-      <ul className= {isMenuOpen ? 'open' : ''}>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/products">Products</NavLink>
-        </li>
-        <li>
-          <NavLink to="/cart">Cart</NavLink>
-        </li>
-        <li>  
-          <NavLink to="/about">About</NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact">Contact</NavLink>
-        </li>
-        <button type="button" className='logout-button'>Logout</button>
+
+      {/* Drawer */}
+      <ul className={`mobile-drawer ${isMenuOpen ? "open" : ""}`}>
+        <span className="close-btn" onClick={() => setIsMenuOpen(false)}>X</span>
+
+        <li><NavLink to="/" onClick={() => setIsMenuOpen(false)}>Home</NavLink></li>
+        <li><NavLink to="/products" onClick={() => setIsMenuOpen(false)}>Products</NavLink></li>
+        <li><NavLink to="/cart" onClick={() => setIsMenuOpen(false)}>Cart</NavLink></li>
+        <li><NavLink to="/contact" onClick={() => setIsMenuOpen(false)}>Contact</NavLink></li>
+
+        <button className="logout-button" onClick={() => setIsMenuOpen(false)}>
+          Logout
+        </button>
+      </ul>
+
+      {/* Desktop Menu */}
+      <ul className="desktop-menu">
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/products">Products</NavLink></li>
+        <li><NavLink to="/cart">Cart</NavLink></li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
+
+        <button className="logout-button">Logout</button>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
