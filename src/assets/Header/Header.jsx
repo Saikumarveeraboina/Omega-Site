079aa484e9119d59365320bef2e29dcc1975a923
Saikumarveeraboina <saikumar96030433@gmail.com>
2025-12-08 12:39:20 +0530

    added 1

--- src/assets/Header/Header.jsx
+++ src/assets/Header/Header.jsx
@@ -1,114 +1,29 @@
-import { Link } from 'react-router-dom'
-import Cookies from 'js-cookie'
+import React from 'react'
+import {Link} from 'react-router-dom'
 import logo from '../../Images/logo-vsk-incl-e.png'
 import './Header.css'
 
-const Header = props => {
-  const onClickLogout = () => {
-    const { history } = props
-    Cookies.remove('jwt_token')
-    history.replace('/login')
-  }
-
+const Header = () => {
   return (
-    <nav className="nav-header">
-      <div className="nav-content">
-        <div className="nav-bar-mobile-logo-container">
-          <Link to="/">
-            <img
-              className="website-logo"
-              src={logo}
-              alt="website logo"
-            />
-          </Link>
-          <button type="button" className="nav-mobile-btn">
-            <img
-              src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-log-out-img.png"
-              alt="nav logout"
-              className="nav-bar-image"
-              onClick={onClickLogout}
-            />
-          </button>
-        </div>
-
-        <div className="nav-bar-large-container">
-          <Link to="/">
-            <img
-              className="website-logo"
-              src={logo}
-              alt="website logo"
-            />
-          </Link>
-          <ul className="nav-menu">
-            <li className="nav-menu-item">
-              <Link to="/" className="nav-link">
-                Home
-              </Link>
-            </li>
-
-            <li className="nav-menu-item">
-              <Link to="/products" className="nav-link">
-                Products
-              </Link>
-            </li>
-
-            <li className="nav-menu-item">
-              <Link to="/cart" className="nav-link">
-                Cart
-              </Link>
-            </li>
-            <li className="nav-menu-item">
-              <Link to="/about" className="nav-link">
-                About
-              </Link>
-            </li>
-            <li className="nav-menu-item">
-              <Link to="/contact" className="nav-link">
-                Contact
-              </Link>
-            </li>
-          </ul>
-          <button
-            type="button"
-            className="logout-desktop-btn"
-            onClick={onClickLogout}
-          >
-            Logout
-          </button>
-        </div>
-      </div>
-      <div className="nav-menu-mobile">
-        <ul className="nav-menu-list-mobile">
-          <li className="nav-menu-item-mobile">
-            <Link to="/" className="nav-link">
-              <img
-                src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-icon.png"
-                alt="nav home"
-                className="nav-bar-image"
-              />
-            </Link>
-          </li>
-
-          <li className="nav-menu-item-mobile">
-            <Link to="/products" className="nav-link">
-              <img
-                src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-icon.png"
-                alt="nav products"
-                className="nav-bar-image"
-              />
-            </Link>
-          </li>
-          <li className="nav-menu-item-mobile">
-            <Link to="/cart" className="nav-link">
-              <img
-                src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-icon.png"
-                alt="nav cart"
-                className="nav-bar-image"
-              />
-            </Link>
-          </li>
-        </ul>
-      </div>
+    <nav>
+      <Link to="/"> <img src={logo} alt="logo" className='website-logo'/> </Link>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/products">Products</Link>
+        </li>
+        <li>
+          <Link to="/cart">Cart</Link>
+        </li>
+        <li>  
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
     </nav>
   )
 }
