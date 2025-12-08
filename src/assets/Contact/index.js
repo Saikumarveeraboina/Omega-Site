144f82ec77794251c698eec0717511cbd867edb3
Saikumarveeraboina <saikumar96030433@gmail.com>
2025-12-08 12:08:59 +0530

    pages addes

---
+++ src/assets/Contact/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../Header/Header'
+
+const Contact = () => {
+    return (
+        <>
+            <Header />
+            <div>Contact</div>
+        </>
+
+    )
+}
+
+export default Contact
