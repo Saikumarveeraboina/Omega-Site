4dcd6adcb56ffaca78bbbf153049c0fd512e74bd
Saikumarveeraboina <saikumar96030433@gmail.com>
2025-12-19 14:55:24 +0530

    omega2

--- src/assets/Header/Header.jsx
+++ src/assets/Header/Header.jsx
@@ -1,93 +1,86 @@
-import React, { useState } from "react";
-import { Link, NavLink } from "react-router-dom";
-import logo from "../../Images/OMSIT.png";
-import { FaPhoneAlt } from 'react-icons/fa'
-import { RiWhatsappFill } from 'react-icons/ri'
-
-import "./Header.css";
+import React from 'react'
+import {Link, NavLink} from 'react-router-dom'
+import Popup from 'reactjs-popup'
+import logo from '../../Images/OMSIT.png'
+import {FaPhoneAlt} from 'react-icons/fa'
+import {RiWhatsappFill} from 'react-icons/ri'
+import {GiHamburgerMenu} from 'react-icons/gi'
+import './Header.css'
 
 const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const phoneNumber = '919246805933'
   const message = 'Hi, I want to know about this college ?'
   const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
     message,
   )}`
 
-
   return (
     <>
+      {/* ---------- TOP BLUE BAR ---------- */}
       <div className="header-container">
         <div className="content">
-          <div>
-            <a href="tel:+919246805933" style={{ textDecoration: 'none', marginLeft: '20px', display: 'flex', alignItems: 'center' }}>
-              <FaPhoneAlt color="white" size={16} />
-              <span style={{ color: 'white', marginLeft: '6px' }}> 
-                +91 9246805933
-              </span>
-            </a>
-          </div>
+          <a
+            href="tel:+919246805933"
+            className="top-link"
+          >
+            <FaPhoneAlt size={16} />
+            <span className="phone-text">+91 9246805933 </span>
+          </a>
 
-            <p style={{ color: 'white' }}>College Code : 2174</p>
+          <p className="college-code">College Code : 2174</p>
 
-            <div style={{ marginLeft: '20px' }}>
-              <a
-                href={whatsappUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ textDecoration: 'none', display: 'flex', alignItems: 'center' }}
-              >
-                <RiWhatsappFill size={20} color="#25D366" style={{ marginRight: '10px' }} />
-                <span style={{ color: 'white', marginRight: '40px' }}>WhatsApp</span>
-              </a>
-
-            </div>
-
-
-          </div>
+          <a
+            href={whatsappUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="top-link"
+          >
+            <RiWhatsappFill size={20} />
+            <span className="whatsapp-text">WhatsApp</span>
+          </a>
         </div>
+      </div>
 
-        <nav className="navbar">
-          {/* Logo */}
-          <Link to="/">
-            <img src={logo} alt="logo" className="website-logo" />
-          </Link>
-          {/* Hamburger */}
-          <div className="menu" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
+      {/* ---------- NAVBAR ---------- */}
+      <nav className="navbar">
+        <Link to="/">
+          <img src={logo} alt="logo" className="website-logo" />
+        </Link>
 
-          {/* Drawer */}
-          <ul className={`mobile-drawer ${isMenuOpen ? "open" : ""}`}>
-            <span className="close-btn" onClick={() => setIsMenuOpen(false)}>X</span>
-
-            <li><NavLink to="/" onClick={() => setIsMenuOpen(false)}>Home</NavLink></li>
-            <li><NavLink to="/products" onClick={() => setIsMenuOpen(false)}>Products</NavLink></li>
-            <li><NavLink to="/cart" onClick={() => setIsMenuOpen(false)}>Cart</NavLink></li>
-            <li><NavLink to="/about" onClick={() => setIsMenuOpen(false)}>About</NavLink></li>
-            <li><NavLink to="/contact" onClick={() => setIsMenuOpen(false)}>Contact</NavLink></li>
-
-            <button className="logout-button" onClick={() => setIsMenuOpen(false)}>
-              Login
+        {/* ---------- MOBILE HAMBURGER (POPUP) ---------- */}
+        <Popup
+          trigger={
+            <button className="hamburger-btn">
+              <GiHamburgerMenu size={22} />
             </button>
-          </ul>
+          }
+          position="right top"
+          closeOnDocumentClick
+        >
+          {close => (
+            <div className="popup-menu">
+              <NavLink to="/" onClick={close}>Home</NavLink>
+              <NavLink to="/products" onClick={close}>Products</NavLink>
+              <NavLink to="/cart" onClick={close}>Cart</NavLink>
+              <NavLink to="/about" onClick={close}>About</NavLink>
+              <NavLink to="/contact" onClick={close}>Contact</NavLink>
+              <button className="logout-button">Login</button>
+            </div>
+          )}
+        </Popup>
 
-          {/* Desktop Menu */}
-          <ul className="desktop-menu">
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/products">Products</NavLink></li>
-            <li><NavLink to="/cart">Cart</NavLink></li>
-            <li><NavLink to="/about">About</NavLink></li>
-            <li><NavLink to="/contact">Contact</NavLink></li>
+        {/* ---------- DESKTOP MENU ---------- */}
+        <ul className="desktop-menu">
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/products">Products</NavLink></li>
+          <li><NavLink to="/cart">Cart</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/contact">Contact</NavLink></li>
+          <button className="logout-button">Login</button>
+        </ul>
+      </nav>
+    </>
+  )
+}
 
-            <button className="logout-button">Login</button>
-          </ul>
-        </nav>
-        
-      </>
-      );
-};
-
-      export default Header;
+export default Header
