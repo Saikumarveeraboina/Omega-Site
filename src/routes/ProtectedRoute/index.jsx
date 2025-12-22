6030c80b78b8818165a700ee9eee508423e26896
Saikumar <saikumar96030433@gmail.com>
2025-12-22 17:21:31 +0530

    Update index.jsx

--- src/routes/ProtectedRoute/index.jsx
+++ src/routes/ProtectedRoute/index.jsx
@@ -2,8 +2,13 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../Auth/AuthContext";
 
 const ProtectedRoute = ({ children }) => {
-  const { user } = useAuth();
-  return user ? children : <Navigate to="/login" />;
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <p>Loading...</p>; // or spinner
+  }
+
+  return user ? children : <Navigate to="/login" replace />;
 };
 
 export default ProtectedRoute;
