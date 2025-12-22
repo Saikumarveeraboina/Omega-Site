b264373dc26a9b9cf412c28bc7e4da6caf90d850
Saikumarveeraboina <saikumar96030433@gmail.com>
2025-12-22 19:23:21 +0530

    updated login

--- src/routes/ProtectedRoute/index.jsx
+++ src/routes/ProtectedRoute/index.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "./Auth/AuthContext";
+import { useAuth } from "../Auth/AuthContext";
 
 const ProtectedRoute = ({ children }) => {
   const { user, loading } = useAuth();
